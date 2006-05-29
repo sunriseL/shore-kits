78c6fa7055b6548d4d8346aab903d1a84025e80a
ngm <ngm@localhost>
2006-05-29 18:08:25 +0000

    Initial commit

---
+++ include/qpipe_panic.h
@@ -0,0 +1,16 @@
+
+#ifndef _QPIPE_PANIC_H
+#define _QPIPE_PANIC_H
+
+#include <cstdlib>       /* for abort() */
+#include "trace/trace.h" /* for TRACE */
+
+
+
+/* exported macros */
+
+#define QPIPE_PANIC() { TRACE(TRACE_ALWAYS, "QPIPE PANIC\n"); abort(); }
+
+
+
+#endif
