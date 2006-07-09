134db4426fa2ec29c7ef6e1c357438a08e524c98
ipandis <ipandis@localhost>
2006-07-09 09:53:43 +0000

    - Forgot to include a file

---
+++ include/workload/repos.h
@@ -0,0 +1,28 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+
+/** @file    : repos.h
+ *  @brief   : Definition of the implemented Jobs (compiled queries)
+ *  @version : 0.1
+ *  @history :
+ 7/7/2006: Initial version
+*/ 
+
+#ifndef __REPOS_H
+#define __REPOS_H
+
+#include "workload/job_directory.h"
+#include "workload/repos/tpch_q1.h"
+
+
+
+/** @fn    : register_jobs()
+ *  @brief : Registers all the implemented Jobs
+ */
+
+void register_jobs() {
+
+    TRACE( TRACE_ALWAYS, " ** SHOULD REGISTER JOBS ** \n");
+}
+
+
+#endif // __REPOS_H
