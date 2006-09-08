43b42a7af36043ece68fa460f468cb5bf09cda58
ngm <ngm@localhost>
2006-09-08 21:12:24 +0000

    - Added O_DIRECT unit test.

---
+++ src/tests/test_odirect.cpp
@@ -0,0 +1,42 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+
+#include "engine/thread.h"
+#include "trace.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+
+int main(int, char**)
+{
+
+  thread_init();
+
+
+  int fd = open("/tmp/odirect_file.txt", O_CREAT|O_WRONLY|O_DIRECT, S_IRUSR|S_IWUSR);
+  if (fd == -1) {
+      TRACE(TRACE_ALWAYS, "open() failed: %s\n",
+            strerror(errno));
+      return -1;
+  }
+  
+
+  write(fd, "hello world", strlen("hello world"));
+  
+
+  int fsync_ret = fsync(fd);
+  if (fsync_ret == -1) {
+      TRACE(TRACE_ALWAYS, "fsync() failed: %s\n",
+            strerror(errno));
+      return -1;
+  }
+
+  TRACE(TRACE_ALWAYS, "Opened file with O_DIRECT flag...\n");
+
+  close(fd);
+  return 0;
+}
