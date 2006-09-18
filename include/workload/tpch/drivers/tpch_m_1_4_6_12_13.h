807deb20c91e9837ebe033aa7731dd183202a41e
ngm <ngm@localhost>
2006-09-18 22:24:11 +0000

    - Added 5 query driver.

---
+++ include/workload/tpch/drivers/tpch_m_1_4_6_12_13.h
@@ -0,0 +1,32 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+#ifndef __TPCH_M_1_4_6_12_13_H
+#define __TPCH_M_1_4_6_12_13_H
+
+#include "workload/driver.h"
+#include "workload/driver_directory.h"
+
+
+ENTER_NAMESPACE(workload);
+
+
+class tpch_m_1_4_6_12_13_driver : public driver_t {
+
+private:
+
+    driver_directory_t* _directory;
+
+public:
+
+    tpch_m_1_4_6_12_13_driver(const c_str& description, driver_directory_t* directory)
+        : driver_t(description),
+          _directory(directory)
+    {
+    }
+    
+    virtual void submit(void* disp);
+    
+};
+
+EXIT_NAMESPACE(workload);
+
+#endif
---
+++ src/workload/tpch/drivers/tpch_m_1_4_6_12_13.cpp
@@ -0,0 +1,46 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+
+#include "scheduler.h"
+#include "workload/tpch/drivers/tpch_m_1_4_6_12_13.h"
+
+#include "workload/common.h"
+
+ENTER_NAMESPACE(workload);
+
+
+void tpch_m_1_4_6_12_13_driver::submit(void* disp) {
+ 
+    thread_t* this_thread = thread_get_self();
+    int selection = this_thread->rand(5);
+
+    driver_t* driver;
+    switch (selection) {
+    case 0:
+        driver = _directory->lookup_driver(c_str("q1"));
+        break;
+        
+    case 1:
+        driver = _directory->lookup_driver(c_str("q4"));
+        break;
+        
+    case 2:
+        driver = _directory->lookup_driver(c_str("q6"));
+        break;
+    case 3:
+        driver = _directory->lookup_driver("q12");
+        break;
+
+    case 4:
+        driver = _directory->lookup_driver("q13");
+        break;
+        
+
+    default:
+        assert(false);
+    }
+
+    TRACE(TRACE_DEBUG, "selection = %d\n", selection);
+    driver->submit(disp);
+}
+
+EXIT_NAMESPACE(workload);
