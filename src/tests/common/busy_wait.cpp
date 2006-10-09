602e0edf7eaeae24ddccde1f6c8c20a69c274f23
ngm <ngm@localhost>
2006-10-09 18:30:32 +0000

    - Added array access code to busy wait loop.

--- src/tests/common/busy_wait.cpp
+++ src/tests/common/busy_wait.cpp
@@ -1,6 +1,16 @@
 
 #include "tests/common/busy_wait.h"
 
+#define ARRAY_SIZE 20
+
+
 void busy_wait(void) {
-  while (1);
+
+  int myarray[ARRAY_SIZE];
+  int i;
+  while (1) {
+    for (i = 0; i < ARRAY_SIZE; i++) {
+      myarray[i] = 2 * myarray[i];
+    }
+  }
 }
