e5f3de310315cb557fa96c3d0f5fb6fe6dd88bbf
ryanjohn <ryanjohn@localhost>
2007-01-25 20:41:30 +0000

    - forgot to add two files supporting safe casts

---
+++ src/workload/common/q6_aggregate.cpp
@@ -0,0 +1,3 @@
+#include "workload/common/q6_aggregate.h"
+
+int const q6_count_aggregate_t::agg_t::ALIGN = sizeof(double);
---
+++ src/workload/tpch/tpch_struct.cpp
@@ -0,0 +1,10 @@
+#include "workload/tpch/tpch_struct.h"
+
+int const tpch_customer_tuple::ALIGN = sizeof(double);
+int const tpch_lineitem_tuple::ALIGN = sizeof(double);
+int const tpch_nation_tuple::ALIGN = sizeof(int);
+int const tpch_orders_tuple::ALIGN = sizeof(double);
+int const tpch_part_tuple::ALIGN = sizeof(double);
+int const tpch_partsupp_tuple::ALIGN = sizeof(double);
+int const tpch_region_tuple::ALIGN = sizeof(int);
+int const tpch_supplier_tuple::ALIGN = sizeof(double);
