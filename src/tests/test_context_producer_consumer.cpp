225b8872109f0e355f9ac1cb5654fdedbc1109e3
ngm <ngm@localhost>
2007-02-06 15:07:00 +0000

    - Removed unused variable warning.

--- src/tests/test_context_producer_consumer.cpp
+++ src/tests/test_context_producer_consumer.cpp
@@ -34,7 +34,7 @@ extern "C" void* producer_main(int arg)
     int i;
     for (i = 0; i < 5; i++) {
 
-        TRACE(TRACE_ALWAYS, "Running producer\n");
+        TRACE(TRACE_ALWAYS, "Running producer with arg = %d\n", arg);
 
         /* Fill the buffer */
         while (buffer_entry_count < NUM_BUFFER_ENTRIES) {
