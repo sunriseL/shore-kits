16b2f48c06b3f22c0ffd688e4589ad6a4140f0d6
ngm <ngm@localhost>
2007-02-10 09:43:44 +0000

    - merge_test now only prints boundary (first tuple, last tuple, and   rollover from late merging).

--- src/workload/tpch/drivers/merge_test.cpp
+++ src/workload/tpch/drivers/merge_test.cpp
@@ -17,11 +17,17 @@ void merge_test_fc(void* sadaptor, void*) {
     TRACE(TRACE_ALWAYS, "Starting...\n");
 
     stage_t::adaptor_t* adaptor = (stage_t::adaptor_t*)sadaptor;
+
+    static const int APPROX_TUPLES_PER_PAGE = 1000;
     
+    int count = 0;
     for (int i = 0; i < 10; i++) {
+        for (int t = 0; t < APPROX_TUPLES_PER_PAGE; t++) {
+            tuple_t tup((char*)&count, sizeof(count));
+            adaptor->output(tup);
+            count++;
+        }
         sleep_interval();
-	tuple_t tup((char*)&i, sizeof(int));
-	adaptor->output(tup);
     }
 }
 
@@ -52,18 +58,46 @@ void merge_test_driver::submit(void* disp) {
     qpipe::query_state_t* qs = dp->query_state_create();
     packet->assign_query_state(qs);
         
+
+    /* sleep before dispatch so we get merged in later */
+    thread_t* self = thread_get_self();
+    sleep(self->rand(20));
+    
+    
     // Dispatch packet
     dispatcher_t::dispatch_packet(packet);
     
+
     tuple_t output;
+    int*    v;
+    int     old_v;
+    bool    first = true;
+    
+
+    /* Only print boundary values (first, end, and rollover from late
+       merging). */
     TRACE(TRACE_ALWAYS, "*** ANSWER ...\n");
     while(out_buffer->get_tuple(output)) {
-        int* v;
+
+        if (!first)
+            old_v = *v;
+
         v = aligned_cast<int>(output.data);
-        TRACE(TRACE_ALWAYS, "*** %d\n", *v);
+
+        if (first)
+            TRACE(TRACE_ALWAYS, "*** first! v: %d\n", *v);
+
+        if (!first && (old_v != ((*v) - 1)))
+            /* non-consecutive values! */
+            TRACE(TRACE_ALWAYS, "*** old_v: %d, v: %d\n", old_v, *v);
+        
+        first = false;
     }
+
+    TRACE(TRACE_ALWAYS, "*** END old_v: %d, v: %d\n", old_v, *v);
 
     dp->query_state_destroy(qs);
 }
+
 
 EXIT_NAMESPACE(workload);
