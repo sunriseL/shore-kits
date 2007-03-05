c33012c1e01b4317f09325a3835721b7f286c997
ngm <ngm@localhost>
2007-03-05 06:07:52 +0000

    - Added tpch_q6_push_filter.h ... not integrated yet.

---
+++ include/workload/tpch/drivers/tpch_q6_push_filter.h
@@ -0,0 +1,16 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+#ifndef __TPCH_Q6_PUSH_FILTER_H
+#define __TPCH_Q6_PUSH_FILTER_H
+
+#include "util.h"
+#include "workload/driver.h"
+
+
+ENTER_NAMESPACE(workload);
+
+DECLARE_DRIVER(tpch_q6_push_filter);
+
+EXIT_NAMESPACE(workload);
+
+
+#endif
