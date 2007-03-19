e559b40a5dca641e28f9cf37e5dcb55e781b8f9e
ngm <ngm@localhost>
2007-03-19 16:59:33 +0000

    - Added util_init.

---
+++ include/util/util_init.h
@@ -0,0 +1,12 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+
+#ifndef __UTIL_INIT_H
+#define __UTIL_INIT_H
+
+
+/* exported functions */
+
+void util_init(void);
+
+
+#endif
---
+++ src/util/util_init.cpp
@@ -0,0 +1,12 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+
+#include "util/util_init.h"
+
+
+/* definitions of exported functions */
+
+void util_init(void)
+{
+
+
+}
