11bbc8105cb59c774f34774f054756a14e43db1d
ngm <ngm@localhost>
2007-03-19 16:46:15 +0000

    - Added busy_delay.

---
+++ include/util/busy_delay.h
@@ -0,0 +1,20 @@
+
+/** @file busy_delay.h
+ *
+ *  @brief Exports busy_delay operations.
+ *
+ *  @author Naju Mancheril (ngm)
+ *
+ *  @bug See busy_delay.c.
+ */
+#ifndef _BUSY_DELAY_H
+#define _BUSY_DELAY_H
+
+
+/* exported functions */
+
+int  busy_delay_init(void);
+void busy_delay(int num_ms);
+
+
+#endif
