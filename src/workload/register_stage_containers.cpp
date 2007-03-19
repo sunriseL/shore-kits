5f8bc6f35ae661f1183e881e3f8a2534b61e2f5d
ngm <ngm@localhost>
2007-03-19 20:40:32 +0000

    - Added Niko's changes to register_stage_containers.cpp in an attempt   to reproduce his segfault. I have been unable to reproduce it on   enceladus (a 4 core, x86 Linux system). I am going to now try on   lomond.
    - DO NOT USE THIS VERSION SINCE IT DISABLES SHARING IN MOST STAGES.

--- src/workload/register_stage_containers.cpp
+++ src/workload/register_stage_containers.cpp
@@ -3,18 +3,34 @@
 #include "stages.h"
 #include "workload/common/register_stage.h"
 
-#define MAX_NUM_CLIENTS 16
+//#define MAX_NUM_CLIENTS 16
+#define MAX_NUM_CLIENTS 128
 
-#define MAX_NUM_TSCAN_THREADS             MAX_NUM_CLIENTS * 2 // Q4 has two scans
+#define MAX_NUM_TSCAN_THREADS             MAX_NUM_CLIENTS * 2 // Q4, Q16 have two scans
 #define MAX_NUM_AGGREGATE_THREADS         MAX_NUM_CLIENTS
-#define MAX_NUM_PARTIAL_AGGREGATE_THREADS MAX_NUM_CLIENTS
+#define MAX_NUM_PARTIAL_AGGREGATE_THREADS MAX_NUM_CLIENTS * 2 // Q1 uses two partial aggregates
 #define MAX_NUM_HASH_JOIN_THREADS         MAX_NUM_CLIENTS
 #define MAX_NUM_FUNC_CALL_THREADS         MAX_NUM_CLIENTS
-#define MAX_NUM_SORT_THREADS              MAX_NUM_CLIENTS
+#define MAX_NUM_SORT_THREADS              MAX_NUM_CLIENTS * 2 // Q16 uses two sorts
+#define MAX_NUM_SORTED_IN_STAGE_THREADS   MAX_NUM_CLIENTS
 
 
 void register_stage_containers() {
 
+    register_stage<tscan_stage_t>(MAX_NUM_TSCAN_THREADS, false);
+    register_stage<aggregate_stage_t>(MAX_NUM_AGGREGATE_THREADS, false);
+    register_stage<partial_aggregate_stage_t>(MAX_NUM_PARTIAL_AGGREGATE_THREADS, false);
+    register_stage<hash_aggregate_stage_t>(MAX_NUM_AGGREGATE_THREADS, false);
+    register_stage<hash_join_stage_t>(MAX_NUM_HASH_JOIN_THREADS, false);
+    register_stage<pipe_hash_join_stage_t>(MAX_NUM_CLIENTS, false);
+    register_stage<func_call_stage_t>(MAX_NUM_FUNC_CALL_THREADS, false);
+    register_stage<sort_stage_t>(MAX_NUM_SORT_THREADS, true);
+
+    register_stage<sorted_in_stage_t>(MAX_NUM_SORTED_IN_STAGE_THREADS, false);
+    register_stage<echo_stage_t>(MAX_NUM_CLIENTS, false);
+    register_stage<sieve_stage_t>(MAX_NUM_CLIENTS, false);
+
+#if 0
     register_stage<tscan_stage_t>(MAX_NUM_CLIENTS);
     register_stage<aggregate_stage_t>(MAX_NUM_CLIENTS);
     register_stage<partial_aggregate_stage_t>(MAX_NUM_CLIENTS);
@@ -31,4 +47,5 @@ void register_stage_containers() {
     //    register_stage<merge_stage_t>(MAX_NUM_CLIENTS);
     //    register_stage<fdump_stage_t>(MAX_NUM_CLIENTS);
     //    register_stage<fscan_stage_t>(MAX_NUM_CLIENTS);
+#endif
 }
