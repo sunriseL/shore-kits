dfb647897600b0285b484d9a4aa226be58d1309a
ngm <ngm@localhost>
2007-04-01 08:08:52 +0000

    - Found a bug with the settings
      static const bool FLUSH_TO_DISK_ON_FULL = true;
      static const bool USE_DIRECT_IO = false;
    
      We get a segfault inside _get_read_page, probably related to the
      _set_read_page bug from earlier. The cause last time was a double
      free (actually, a double delete) error do to incorrectly settings
      the _free_read_page flag.

--- src/core/tuple_fifo.cpp
+++ src/core/tuple_fifo.cpp
@@ -18,7 +18,7 @@ ENTER_NAMESPACE(qpipe);
 
 
 
-/* debugging */
+/* debugging... */
 static int TRACE_MASK_WAITS = TRACE_COMPONENT_MASK_NONE;
 static int TRACE_MASK_DISK  = TRACE_COMPONENT_MASK_NONE;
 static const bool FLUSH_TO_DISK_ON_FULL = true;
