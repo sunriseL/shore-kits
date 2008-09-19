eb38e1f5728b6a969c48d263e598639d0287809d
ryanjohn <ryanjohn@localhost>
2008-09-19 14:18:49 +0000

    - added a single-reader, multiple-writer action queue

---
+++ include/dora/srmwqueue.h
@@ -0,0 +1,63 @@
+/* -*- mode:C++; c-basic-offset:4 -*- */
+
+
+/** @file srmwqueue.h
+ *
+ *  @brief A single-reader, multiple-writer queue.
+ *
+ *  Queue size is unbounded and the reader spins while waiting for new
+ *  elements to arrive.
+ *
+ *  @author Ryan Johnson (ryanjohn)
+ */
+
+#include <sthread.h>
+#include <vector>
+
+#include "util.h"
+#include "dora/action.h"
+
+ENTER_NAMESPACE(dora);
+
+template<class action_t>
+struct srmwqueue {
+    typedef std::vector<action_t*> action_list;
+    action_list _for_writers;
+    action_list _for_readers;
+    action_list::iterator _read_pos;
+    mcs_lock _lock;
+    int volatile _empty;
+
+    srmwqueue() : _empty(true) { }
+    
+    action_t* pop() {
+	if(_read_pos == _for_readers.end() && !wait_for_input())
+	    return NULL;
+
+	return *(_read_pos++);
+    }
+
+    void push(action_t* a) {
+	CRITICAL_SECTION(cs, _lock);
+	_for_writers.push_back(a);
+	_empty = false;
+    }
+  
+    bool wait_for_input(WorkerControl volatile* wc) {
+	while(*&_empty) {
+	    if(*wc != WC_ACTIVE)
+		return false;
+	}
+    
+	{
+	    CRITICAL_SECTION(cs, _lock);
+	    _for_readers.clear();
+	    _for_writers.swap(_for_readers);
+	    _empty = true;
+	}
+    
+	_read_pos = _for_readers.begin();
+	return true;
+    }
+  
+};
