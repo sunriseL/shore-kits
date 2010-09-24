53ee40c1fc5a8df83d726f2b33890d5443a8f601
Ippokratis Pandis <ippokratis.pandis@epfl.ch>
2010-09-24 22:04:10 +0200

    - Updating autogen with information about the configuration options

--- autogen.sh
+++ autogen.sh
@@ -6,3 +6,38 @@ aclocal
 autoconf -f
 automake --add-missing --force
 
+## Configuration options
+echo ------------------------------------------------------------
+echo
+echo Run
+echo     ./configure [CONFIGURATIONS] [COMPILATION] SHORE_HOME=dir
+echo
+echo     ./configure --help 
+echo            to see the options
+echo    
+echo Supported configuration options
+echo --enable-shore6  -  If compiling against shore-sm-6.X.X
+echo --enable-dora    -  Includes DORA files, defines CFG_DORA
+echo --enable-elr     -  Defines CFG_ELR
+echo --enable-flusher -  Defines CFG_FLUSHER
+echo --enable-sli     -  Defines CFG_SLI
+echo --enable-qpipe   -  Includes QPipe files, defines CFG_QPIPE
+echo --enable-bt      -  Enables backtracing facility. defines CFG_BT
+echo --enable-simics  -  Adds the simics MAGIC instructions. defines CFG_SIMICS
+echo   
+echo   
+echo There are 3 supported compilation options
+echo --enable-debug      -  Compile for debugging, e.g., -g
+echo --enable-profile    -  Compile for profiling, e.g., -pg for oprofile
+echo --enable-dbgsymbols -  Compile with debug symbols, e.g., -ggdb
+echo
+echo If none of them is enabled then the default compilation will be with
+echo the maximum optimizations possible, e.g., -O3 or -xO4
+echo
+echo For SOLARIS, we suggest that you use --enable-dependendency-tracking and CC
+echo     ./configure  CXX=CC --enable-dependency-tracking 
+echo
+echo After you configure, run
+echo     make
+echo
+echo ------------------------------------------------------------
